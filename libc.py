0452a0c84c201b34ab83b14f21c16e4300059b86
Vladimir Kirillov <proger@wilab.org.ua>
2011-06-12 03:33:40 +0300

    kmem_read(); read_udpmem test

--- libc.py
+++ libc.py
@@ -1,12 +1,30 @@
 import os
 import ctypes
+import struct
+from commands import getoutput as go
 
 libc = ctypes.CDLL('/lib/libc.so.6')
 
 lseek = libc.lseek64
 u64 = ctypes.c_uint64
 
+# XXX: need CAP_SYS_RAWIO
 kmem = os.open('/dev/kmem', 0, 0)
 
-#  ffffffff81cc07a0 D sysctl_udp_mem    long[3]
-off = u64(0xffffffff81cc07a0)
+# TODO: percpu()
+
+def kmem_read(fd, addr, size):
+    off = u64(addr)
+
+    lseek(fd, off, os.SEEK_SET)
+    return os.read(fd, size)
+
+def read_udpmem():
+    #  ffffffff81cc07a0 D sysctl_udp_mem    long[3]
+
+    data = struct.unpack('@lll',
+            kmem_read(kmem, 0xffffffff81cc07a0L, 8 * 3))
+
+    # sysctl net.ipv4.udp_mem
+    assert map(int, go('sysctl net.ipv4.udp_mem').split('=')[1].split()) == list(data)
+    return data
