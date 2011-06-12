07f06dad78304e0009e817a79c72a7c2af473de2
Vladimir Kirillov <proger@wilab.org.ua>
2011-06-12 04:09:23 +0300

    split tests

--- kmem.py
+++ kmem.py
@@ -1,7 +1,6 @@
 import os
 import ctypes
 import struct
-from commands import getoutput as go
 
 libc = ctypes.CDLL('/lib/libc.so.6')
 
@@ -17,7 +16,6 @@ kmem = os.open('/dev/kmem', 0, 0)
 
 def kmem_read(addr, size):
     off = u64(addr)
-
     lseek(kmem, off, os.SEEK_SET)
     return os.read(kmem, size)
 
@@ -31,23 +29,10 @@ with open('/proc/kallsyms') as ksyms:
             if ty == 'D')
     kallsyms = dict(data)
 
-def per_cpu_offset(cpuno=0):
-    # ffffffff81cb8760 D __per_cpu_offset
-    addr = kallsyms['__per_cpu_offset']
-    return kmem_read_ptr(addr + cpuno * ptrsize)
+per_cpu_offset = lambda cpuno=0: kmem_read_ptr(kallsyms['__per_cpu_offset'] + cpuno * ptrsize)
 
 def per_cpu(sym, cpuno=0):
     addr = kallsyms[sym]
     off = per_cpu_offset(cpuno)
 
     return kmem_read_ptr(off + addr)
-
-def read_udpmem():
-    #  ffffffff81cc07a0 D sysctl_udp_mem    long[3]
-
-    data = struct.unpack('@lll',
-            kmem_read(kallsyms['sysctl_udp_mem'], 8 * 3))
-
-    # sysctl net.ipv4.udp_mem
-    assert map(int, go('sysctl net.ipv4.udp_mem').split('=')[1].split()) == list(data)
-    return data
---
+++ test.py
@@ -0,0 +1,19 @@
+from kmem import *
+from commands import getoutput as go
+
+def read_udpmem():
+    #  ffffffff81cc07a0 D sysctl_udp_mem    long[3]
+
+    data = struct.unpack('@lll',
+            kmem_read(kallsyms['sysctl_udp_mem'], 8 * 3))
+
+    # sysctl net.ipv4.udp_mem
+    assert map(int, go('sysctl net.ipv4.udp_mem').split('=')[1].split()) == list(data)
+    return data
+
+def read_current_task():
+    assert per_cpu('current_task', 0) != per_cpu('current_task', 1)
+
+if __name__ == '__main__':
+    print read_udpmem()
+    read_current_task()
